df3442044fdd622a67dac0adcfbfb9f30799dbe2
Daniel Safonov <imdaneel1798@gmail.com>
2019-01-24 15:41:46 +0300

    1. Spring Greeting - обработка GET-запроса - заменил Thymeleaf на Mustache

--- src/main/java/Main/GreetingController.java
+++ src/main/java/Main/GreetingController.java
@@ -5,15 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller //Класс-контроллер обрабатывает HTTP-запросы
 public class GreetingController {
 
     @GetMapping("/greeting") //Обрабатывает GET запросы на /greeting вызовом метода greeting()
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model){
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Map<String, Object> model){
         //Аннотация описывает параметр запроса name (не обязателен, стандартное значение - World) (?name=Daniel)
-        //Второй входной параметр метода - Model (модель)
+        //Второй входной параметр метода - карта строка-объект - Model (модель)
 
-        model.addAttribute("name", name); //Передача в модель параметра name
+        model.put("name", name); //Передача в модель параметра name
         return "greeting"; //Возвращает имя View (веб-страницы)
 
     }
