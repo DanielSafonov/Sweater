e990044712155ba46c1fbed3d5547985be99f121
DanielSafonov <imdaneel1798@gmail.com>
2019-02-02 22:07:14 +0300

    Файл отдает файлы (отображаются в списке сообщений)

--- src/main/java/com/DanielSafonov/Sweater/config/MvcConfig.java
+++ src/main/java/com/DanielSafonov/Sweater/config/MvcConfig.java
@@ -1,14 +1,30 @@
 package com.DanielSafonov.Sweater.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration //Файл конфигурации для привязки login к /login (привязка view-контроллеров к view)
 public class MvcConfig implements WebMvcConfigurer {
+    //Получает путь из application.properties и вставляет его в переменную
+    @Value("${upload.path}")
+    private String uploadPath;
 
+    //Привязка view к controller (сгенерированному автоматически)
     public void addViewControllers(ViewControllerRegistry registry){
         registry.addViewController("/login").setViewName("login");
         //Все остальные View уже замеппили
     }
+
+    //Каждое обращение к серверу по пути img перенаправляет все запросы по другому пути (протокол file)
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("file://" + uploadPath + "/");
+    }
+
+
+
 }
